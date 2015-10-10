6053ecadca8886640548c543e79a894fde651680
kunal <kunaladhia01@gmail.com>
2015-10-10 19:34:41 -0400

    3.0.1

--- student/admin.py
+++ student/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import UserInfo
 # Register your models here.
-admin.site.register(UserInfo)
+class UserAdmin(admin.ModelAdmin):
+	fields = ['user', 'grade', 'class_of', 'balance',  'pub_date']
+        list_display = ['user', 'grade', 'class_of', 'balance',  'pub_date']
 
+admin.site.register(UserInfo, UserAdmin)
--- student/models.py
+++ student/models.py
@@ -4,9 +4,15 @@ from django.contrib.auth.models import User
 class UserInfo(models.Model):
     user = models.ForeignKey(User)
     class_of = models.IntegerField()
-    pub_date = models.DateTimeField( auto_now=True )
+    #username = user.username
+    #fname = user.first_name
+    #lname = user.last_name
+    #email = user.email
+    #Staff = user.is_staff
+    pub_date = models.DateTimeField('date modified' )
     grade = models.IntegerField()
-    def __str__(self):
-	return self.user.username
+    balance = models.DecimalField(max_digits=6, decimal_places=2)
+    def __unicode__(self):
+	return str(self.user.username)
 
 # Create your models here.
